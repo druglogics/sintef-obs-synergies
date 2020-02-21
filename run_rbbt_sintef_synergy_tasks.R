f615b296e6a14e3c9fd5d224e6db3265d684f943
john <bblodfon@gmail.com>
2020-02-21 16:01:27 +0100

    finish script for analysis

--- run_rbbt_sintef_synergy_tasks.R
+++ run_rbbt_sintef_synergy_tasks.R
@@ -1,46 +1,79 @@
-# source some functions
+library(dplyr)
 source(file = "fun.R")
 
-# Find the Gold-Standard synergies per cell line
+##################################################
+# Find the Gold-Standard synergies per cell line #
+##################################################
+
 cell.lines = c("A498", "AGS", "Colo205", "DU145", "MDA-MB-468", "SF295", "SW620", "UACC62")
 gs.list = list()
 for (cell.line in cell.lines) {
   gs.list[[cell.line]] = run.sintef.task(task = "GS", cell.line)
 }
 
-# save the list
+# save the Gold-Standard list
 saveRDS(gs.list, file = "gs_list.rds")
 
-df = tibble(task = character(), cell_line = character(),
-  synergy_model = character(), params = character(), score = numeric())
+########################################################
+# Score all SINTEF synergy tasks across parameter grid #
+########################################################
 
 # General data object
 data.list = list()
+index = 1
 
-# General Parameter
+# Parameters
 ci_methods = c("bliss", "hsa")
-
-# from all (loose) to none (strict threshold)!
-excess_thresholds = seq(from = 0, to = -0.5, by = -0.01)
-proportional_excess_thresholds = seq(from = 0, to = 0.8, by = 0.05)
+excess_thresholds = seq(from = 0, to = -0.5, by = -0.01) # ranging from loose to strict threshold!
 
 # AvgBliss/HSA method
 task.name = "Avg"
+
 for (cell.line in cell.lines) {
   gset = gs.list[[cell.line]]
-  #for (ci_method in ci_methods) {
-  # for (excess_threshold in excess_thresholds) {
-  mset = run.sintef.task(task = task.name, cell.line = cell.line, ci_method = ci_methods[1], excess_threshold = excess_thresholds[1])
-  df %>% add_row(task = task.name, cell_line = cell.line, synergy_model = ci_method, params = paste0("et_", excess_threshold), score = get.score(gset, mset))
-  if (is_empty(mset)) break
-  #}
-  #}
+  for (ci_method in ci_methods) {
+    for (excess_threshold in excess_thresholds) {
+      mset = run.sintef.task(task = task.name, cell.line = cell.line, ci_method = ci_method, excess_threshold = excess_threshold)
+      data.list[[index]] = tibble(task = task.name, cell_line = cell.line, ci_method = ci_method, excess_threshold = excess_threshold, f1.score = get.score(gset, mset))
+      index = index + 1
+    }
+  }
 }
 
 # ConExcessBliss/HSA method
-# from 1 (loose) to 5 consecutive points (max is 5 in each curve - so more strict)
-consecutive_excess_count = 1:5
+consecutive_excess_counts = 1:5 # more points => more strict, also max is 5 in each curve
+task.name = "ConExcess"
 
+for (cell.line in cell.lines) {
+  gset = gs.list[[cell.line]]
+  for (ci_method in ci_methods) {
+    for (consecutive_excess_count in consecutive_excess_counts) {
+      for (excess_threshold in excess_thresholds) {
+        mset = run.sintef.task(task = task.name, cell.line = cell.line, ci_method = ci_method, consecutive_excess_count = consecutive_excess_count, excess_threshold = excess_threshold)
+        data.list[[index]] = tibble(task = task.name, cell_line = cell.line, ci_method = ci_method, excess_threshold = excess_threshold, consecutive_excess_count = consecutive_excess_count, f1.score = get.score(gset, mset))
+        index = index + 1
+      }
+    }
+  }
+}
 
 # ConPropExcessBliss/HSA method
+proportional_excess_thresholds = seq(from = 0, to = 0.8, by = 0.05) # ranging from loose to more strict!
+task.name = "ConPropExcess"
+
+for (cell.line in cell.lines) {
+  gset = gs.list[[cell.line]]
+  for (ci_method in ci_methods) {
+    for (consecutive_excess_count in consecutive_excess_counts) {
+      for (proportional_excess_threshold in proportional_excess_thresholds) {
+        mset = run.sintef.task(task = task.name, cell.line = cell.line, ci_method = ci_method, consecutive_excess_count = consecutive_excess_count, proportional_excess_threshold = proportional_excess_threshold)
+        data.list[[index]] = tibble(task = task.name, cell_line = cell.line, ci_method = ci_method, proportional_excess_threshold = proportional_excess_threshold, consecutive_excess_count = consecutive_excess_count, f1.score = get.score(gset, mset))
+        index = index + 1
+      }
+    }
+  }
+}
+
+res.data = bind_rows(data.list)
+saveRDS(res.data, file = "res_data.rds")
 
