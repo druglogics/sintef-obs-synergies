d3b0648b37336fca58fd085173a21397501dd77c
john <bblodfon@gmail.com>
2020-02-21 16:08:39 +0100

    change output filename to 'index.html'

--- render.sh
+++ render.sh
@@ -1,2 +1,2 @@
-Rscript -e "rmarkdown::render(input = 'sintef_obs_synergies.Rmd', output_format = 'html_document', output_dir = 'docs')"
-Rscript -e "utils::browseURL(url = '/home/john/repos/druglogics/sintef-obs-synergies/docs/sintef_obs_synergies.html')"
+Rscript -e "rmarkdown::render(input = 'sintef_obs_synergies.Rmd', output_format = 'html_document', output_file = 'index.html', output_dir = 'docs')"
+Rscript -e "utils::browseURL(url = '/home/john/repos/druglogics/sintef-obs-synergies/docs/index.html')"
