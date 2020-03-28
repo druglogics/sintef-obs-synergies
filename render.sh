396e8886b452b53f3e1073b704c09a0d48e9febb
john <bblodfon@gmail.com>
2020-03-28 02:43:41 +0100

    change to relative link

--- render.sh
+++ render.sh
@@ -1,2 +1,2 @@
 Rscript -e "rmarkdown::render(input = 'sintef_obs_synergies.Rmd', output_format = 'html_document', output_file = 'index.html', output_dir = 'docs')"
-Rscript -e "utils::browseURL(url = '/home/john/repos/druglogics/sintef-obs-synergies/docs/index.html')"
+Rscript -e "utils::browseURL(url = 'docs/index.html')"
